bb08737b96a03f7e350d71e4e369a4468ef99df3
김동우 <cjalex@naver.com>
2024-12-29 09:10:24 +0900

    Initial commit

---
+++ src/main/java/com/dongwoo/cs/DailyCsApplication.java
@@ -0,0 +1,13 @@
+package com.dongwoo.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyCsApplication.class, args);
+	}
+
+}
